b2c8d712ea278a02b14d2552747056f6a82ac918
Muhammad Ahsan <ahsanirfan961@gmail.com>
2024-08-11 23:57:14 +0500

    Trained for only 30 epochs

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import torch
 # Load YOLOv8 model
-model = YOLO("yolov8x.pt").to('cuda')  # Ensure the model file path is correct
+model = YOLO("runs/detect/train2/weights/best.pt").to('cuda')  # Ensure the model file path is correct
 
 # Track objects in a video file
 results=model.track(rf'cctv.mp4',save=True,classes=[0],persist=True, tracker="bytetrack.yaml",device='cuda')
